d19a0d979f918d7a7abb4ee7a3a6e616cbcec0a4
Saman <saman.ashoori@outlook.com>
2024-12-06 23:23:22 +0000

    did some updating and shit

--- seconds.js
+++ seconds.js
@@ -1,15 +1,26 @@
 let secondsLeft = document.getElementById('SecondsLeft');
 let secondsLived = document.getElementById('SecondsLived');
+let percentLived = document.getElementById('PercentLived');
 
-let dateBorn = new Date('12/29/2000');
+let dateBorn;
+let dateDepart = new Date('12/29/2080');
 
-var dateNow = new Date();
-var dateDepart = new Date('12/29/2080')
-dif = Math.abs(dateBorn.getTime() - dateNow.getTime()) /1000
-dif2 = Math.abs(dateNow.getTime() - dateDepart.getTime()) / 1000
-let percent
+function setBirthday() {
+    let userBirthday = document.getElementById('userBirthday').value;
+    dateBorn = new Date(userBirthday);
+    updateDisplay();
+}
 
-var display = setInterval(
-    secondsLived.innerHTML = "Seconds Lived :" + dif ,1000)
-var display1 = setInterval(
-    secondsLeft.innerHTML = "Seconds left :" + dif2 ,1000)
+function updateDisplay() {
+    var display = setInterval(function() {
+        var dateNow = new Date();
+        let dif = Math.abs(dateBorn.getTime() - dateNow.getTime()) / 1000;
+        let dif2 = Math.abs(dateNow.getTime() - dateDepart.getTime()) / 1000;
+        let totalLifeSpan = Math.abs(dateDepart.getTime() - dateBorn.getTime()) / 1000;
+        let percent = ((dif / totalLifeSpan) * 100).toFixed(4);
+        
+        secondsLived.innerHTML = "Seconds Lived: " + Math.floor(dif);
+        secondsLeft.innerHTML = "Seconds Left: " + Math.floor(dif2);
+        percentLived.innerHTML = "Percent Lived: " + percent + "%";
+    }, 1000);
+}
