140e1f246bfdbf6632f2025e4ee9662ded2c02d7
samanashoori <39128971-samanashoori@users.noreply.replit.com>
2025-02-17 23:16:50 +0000

    User checkpoint: Fix: Allow requests from Replit's Janeway during development.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    allowedHosts: [
+      '3ee5f85a-f792-48c2-841d-7893d1d68ef0-00-76dujhavb5g1.janeway.replit.dev'
+    ]
   }
 })
